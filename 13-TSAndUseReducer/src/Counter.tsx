9c561841ac94944623ed566c25f8af636c898da6
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-05-10 20:10:34 -0700

    Went over TS and useReducer hooks

--- 13-TSAndUseReducer/src/Counter.tsx
+++ 13-TSAndUseReducer/src/Counter.tsx
@@ -1,27 +1,63 @@
-import { ReactNode, useState } from 'react';
+import { ReactNode, useState, useReducer, ChangeEvent } from 'react';
+
+const initialState = {
+    count: 0,
+    text: ''
+}
+
+const enum REDUCER_ACTION_TYPE {
+    INCREMENT,
+    DECREMENT,
+    NEW_INPUT,
+}
+
+type ReducerAction = {
+    type: REDUCER_ACTION_TYPE,
+    payload?: string, 
+}
+
+function reducer(state: typeof initialState, action: ReducerAction): typeof initialState {
+    switch(action.type) {
+        case REDUCER_ACTION_TYPE.INCREMENT:
+            return {...state, count: state.count + 1}
+        case REDUCER_ACTION_TYPE.DECREMENT:
+            return {...state, count: state.count - 1}
+        case REDUCER_ACTION_TYPE.NEW_INPUT:
+            return {...state, text: action.payload ?? ''}
+        default:
+            throw new Error();
+    }
+}
 
 type ChildrenType = {
     children: (num: number) => ReactNode;
 }
 
 export default function Counter({ children }: ChildrenType) {
-    const [count, setCount] = useState<number>(1);
+    // const [count, setCount] = useState<number>(1);
+    const [state, dispatch] = useReducer(reducer, initialState);
     
     function increment() {
-        setCount(prevCount => prevCount + 1);
+        dispatch({type: REDUCER_ACTION_TYPE.INCREMENT});
     }
 
     function decrement() {
-        setCount(prevCount => prevCount - 1);
+        dispatch({type: REDUCER_ACTION_TYPE.DECREMENT});
+    }
+
+    function handleTextInput(e: ChangeEvent<HTMLInputElement>) {
+        dispatch({type: REDUCER_ACTION_TYPE.NEW_INPUT, payload: e.target.value});
     }
 
     return (
         <>
-            <h1>{children(count)}</h1>
+            <h1>{children(state.count)}</h1>
             <div>
                 <button onClick={increment}>+</button>
                 <button onClick={decrement}>-</button>
             </div>
+            <input type='text' onChange={handleTextInput} />
+            <h2>{state.text}</h2>
         </>
     )
 }
