78cdd5d1d62d875dc66af8b0076417295940644b
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-05-16 19:37:11 -0700

    Added initial components

--- 14-TypeScriptProject2/src/App.tsx
+++ 14-TypeScriptProject2/src/App.tsx
@@ -1,6 +1,22 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ProductList from "./components/ProductList";
+import Cart from "./components/Cart";
+import { useState } from "react";
+
 export default function App() {
-  return (
-    <></>
+
+  const [viewCart, setViewCart] = useState<boolean>(false);
+
+  const pageContent = viewCart ? <Cart /> : <ProductList />;
+  const content = (
+    <>
+      <Header viewCart={viewCart} setViewCart={setViewCart} />
+      {pageContent}
+      <Footer viewCart={viewCart} />
+    </>
   )
+
+  return content;
 }
 
