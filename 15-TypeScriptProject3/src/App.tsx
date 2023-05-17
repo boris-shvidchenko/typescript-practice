0a5733d7f78030aea2bcae14567214c16ce461c6
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-05-17 14:58:35 -0700

    Added folder for new TS project, set up folder/file structure, cleaned up code

---
+++ 15-TypeScriptProject3/src/App.tsx
@@ -0,0 +1,13 @@
+// Components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
