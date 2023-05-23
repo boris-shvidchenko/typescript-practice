a86110eb689b4e41480371b58f1832b1bd9c508c
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-05-22 19:48:00 -0700

    Added styles to individual item compnents

--- 15-TypeScriptProject3/src/components/Item.tsx
+++ 15-TypeScriptProject3/src/components/Item.tsx
@@ -10,7 +10,13 @@ export default function Item({ name, img, price }: ItemType) {
         <div className='item-main'>
             <p className='item-name'>{name}</p>
             <img className='item-img' src={img} alt={'Image of ' + name} />
-            <p className='item-price'>Cost: ${price}</p>
+            <section className='item-section'>
+                <p className='item-price'>Cost: ${price}</p>
+                <div className='item-div'>
+                    <button className='item-btn'>Add to cart</button>
+                    <button className='item-btn'>Remove</button>
+                </div>
+            </section>
         </div>
     )
 }
