7bb75599638c7022c3b2ec8f97b68d230147eedf
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-05-22 20:35:36 -0700

    Began creating context/reducer for state. Currently working on setting up types and state before continuing to UI

--- 15-TypeScriptProject3/src/App.tsx
+++ 15-TypeScriptProject3/src/App.tsx
@@ -7,20 +7,58 @@ import Cart from "./pages/Cart";
 import Careers from "./pages/Careers";
 // Router
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// Hooks
+import { createContext, useReducer } from 'react';
+// Context
+// import { state, dispatch } from "./context/storeContext";
+
+
+// // Initial State
+// type initialStateType = {
+//   items: any[];
+// }
+
+// export const StoreContext = createContext<initialStateType | null>(null);
 
 export default function App() {
+
+    // const initialState: initialStateType = {
+    //     items: [],
+    // }
+
+    // // Action type
+    // type actionType = {
+    //     type: string,
+    //     payload: any,
+    // }
+
+    // // Reducer
+    // const [state, dispatch] = useReducer(reducer, initialState);
+
+    // function reducer(state: initialStateType, action: actionType) {
+    //     switch (action.type) {
+    //         case 'UPDATE_ITEMS':
+    //             return {...state, items: action.payload}
+    //         default:
+    //             return state;
+    //     }
+    // }
+
+
   return (
-    <BrowserRouter> 
-      <main className='main-app'>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Store />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/careers' element={<Careers />} />
-        </Routes>
-        <Footer />
-      </main>
-    </BrowserRouter>
+    <StoreContext.Provider value={{state, dispatch}}>
+      <BrowserRouter> 
+        <main className='main-app'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Store />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/careers' element={<Careers />} />
+          </Routes>
+          <Footer />
+        </main>
+      </BrowserRouter>
+    </StoreContext.Provider>
   )
 }
 
---
+++ 15-TypeScriptProject3/src/context/storeContext.tsx
@@ -0,0 +1,32 @@
+// import { useReducer } from 'react';
+
+// // Initial State
+// type initialStateType = {
+//     items: any[];
+// }
+
+// const initialState: initialStateType = {
+//     items: [],
+// }
+
+// // Action type
+// type actionType = {
+//     type: string,
+//     payload: any,
+// }
+
+// // Reducer
+// const [state, dispatch] = useReducer(reducer, initialState);
+
+// function reducer(state: initialStateType, action: actionType) {
+//     switch (action.type) {
+//         case 'UPDATE_ITEMS':
+//             return {...state, items: action.payload}
+//         default:
+//             return state;
+//     }
+// }
+    
+
+// // Exports
+// export { state, dispatch };
