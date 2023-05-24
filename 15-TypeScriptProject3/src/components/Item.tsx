4fa4b9ecbd4e72e56be64572d693498408b0db15
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-05-24 16:03:00 -0700

    Updated function, included further steps

--- 15-TypeScriptProject3/src/components/Item.tsx
+++ 15-TypeScriptProject3/src/components/Item.tsx
@@ -6,17 +6,32 @@ type ItemType = {
     img: string, 
     key: string, 
     price: number
-}
+};
 
 export default function Item({ name, img, price }: ItemType) {
 
     const {state, dispatch} = useContext(StoreContext);
 
-    // Temporary function
-    function displayState() {
-        console.log('1: ', state);
+    // Adds item to state
+    function addItem() {
+
+        // Plan:
+        // if list with name not in state
+        //      then add list
+        // else 
+        //      then + quantity (last item in list)
+
+        // Remove function will be opposite 
+
+        // state.items.forEach(item => {
+            
+        // });
+        // [ [ name, img, price, qnty ],[] ]
+        // console.log(itemInState);
         dispatch({ type: 'UPDATE_ITEMS', payload: [name, img, price, 1] });
-        console.log('2: ', state);
+        
+        // For testing 
+        console.log(state);
     }
 
     return (
@@ -26,7 +41,7 @@ export default function Item({ name, img, price }: ItemType) {
             <section className='item-section'>
                 <p className='item-price'>Cost: ${price}</p>
                 <div className='item-div'>
-                    <button onClick={displayState} className='item-btn'>Add to cart</button>
+                    <button onClick={addItem} className='item-btn'>Add to cart</button>
                     <button className='item-btn'>Remove</button>
                 </div>
             </section>
