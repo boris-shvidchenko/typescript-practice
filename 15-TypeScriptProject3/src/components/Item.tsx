923cd4c7c41069c6ad113fc5afcff5782dda8afc
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-05-31 15:13:55 -0700

    Add item function fixed

--- 15-TypeScriptProject3/src/components/Item.tsx
+++ 15-TypeScriptProject3/src/components/Item.tsx
@@ -14,22 +14,25 @@ export default function Item({ name, img, price }: ItemType) {
 
     // Adds item to state
     function addItem() {
-
         const itemCheck = state.items.filter(i => i[0] === name);
-        if(itemCheck.length === 0) {
+        if (itemCheck.length === 0) {
             dispatch({ type: 'UPDATE_ITEMS', payload: [name, img, price, 1] });
         } else {
             // Adds one to quantity
-            const newArray = [itemCheck[0][0], itemCheck[0][1], itemCheck[0][2], itemCheck[0][3]++];
+            const newArray: ItemType | any = [itemCheck[0][0], itemCheck[0][1], itemCheck[0][2], itemCheck[0][3]++];
             const indexOfItem: number = state.items.indexOf(itemCheck[0]); 
-            // dispatch({ type: 'UPDATE_ITEMS', payload: [...state.items, state.items[indexOfItem] = newArray] });
-
-            // Next step, update state with new array here, running into issues
-            // Then, add the remove functionm which is the opposite if this function, also add circle with number of items in cart to cart navigation in header
         }
+    }
 
-        // For testing 
-        console.log(state.items);
+    function removeItem() {
+        // const itemCheck = state.items.filter(i => i[0] === name);
+        // if (itemCheck.length === 0) {
+        //     dispatch({ type: 'UPDATE_ITEMS', payload: [name, img, price, 1] });
+        // } else {
+        //     // Adds one to quantity
+        //     const newArray: ItemType | any = [itemCheck[0][0], itemCheck[0][1], itemCheck[0][2], itemCheck[0][3]++];
+        //     const indexOfItem: number = state.items.indexOf(itemCheck[0]); 
+        // }
     }
 
     return (
@@ -40,7 +43,7 @@ export default function Item({ name, img, price }: ItemType) {
                 <p className='item-price'>Cost: ${price}</p>
                 <div className='item-div'>
                     <button onClick={addItem} className='item-btn'>Add to cart</button>
-                    <button className='item-btn'>Remove</button>
+                    <button onClick={removeItem} className='item-btn'>Remove</button>
                 </div>
             </section>
         </div>
